cf45458a7aecc9349da25d11ca8d072ecaf5ca7b
DerekPeacock <derek.peacock@yahoo.com>
2019-05-09 14:04:26 +0100

    Basic Sketch program completed

--- CO453C_Graphics/SketchForm.Designer.cs
+++ CO453C_Graphics/SketchForm.Designer.cs
@@ -28,20 +28,40 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.keyLabel = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // keyLabel
+            // 
+            this.keyLabel.AutoSize = true;
+            this.keyLabel.Location = new System.Drawing.Point(607, 388);
+            this.keyLabel.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.keyLabel.Name = "keyLabel";
+            this.keyLabel.Size = new System.Drawing.Size(0, 26);
+            this.keyLabel.TabIndex = 0;
             // 
             // SketchForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(692, 423);
+            this.Controls.Add(this.keyLabel);
+            this.DoubleBuffered = true;
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(6);
             this.Name = "SketchForm";
             this.Text = "Derek\'s Etch-a-Sketch";
             this.Load += new System.EventHandler(this.SketchForm_Load);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.SketchForm_Paint);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.SketchForm_KeyDown);
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.SketchForm_KeyPress);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label keyLabel;
     }
 }
--- CO453C_Graphics/SketchForm.cs
+++ CO453C_Graphics/SketchForm.cs
@@ -12,8 +12,14 @@ namespace CO453C_Graphics
 {
     public partial class SketchForm : Form
     {
+        Bitmap backImage;
+
         Color penColor = Color.Black;
         Color brushColor = Color.Blue;
+
+        int x = 100;
+        int y = 100;
+        int move = 10;
 
         Brush myBrush;
 
@@ -29,11 +35,62 @@ namespace CO453C_Graphics
 
         private void SketchForm_Load(object sender, EventArgs e)
         {
-            Bitmap bm = new Bitmap(Width, Height);
-            Graphics g = Graphics.FromImage(bm);
+            backImage = new Bitmap(Width, Height);
+            Graphics g = Graphics.FromImage(backImage);
 
-            g.FillEllipse(myBrush, 100, 100, 20, 20);
-            this.BackgroundImage = bm;
+            g.Clear(Color.Yellow);
+            g.FillEllipse(myBrush, x, y, 20, 20);
+            this.BackgroundImage = backImage;
+        }
+
+        private void SketchForm_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            //char key = (char)e.KeyChar;
+            //keyLabel.Text = "Key = " + key;
+        }
+
+        private void SketchForm_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.KeyData == Keys.Right)
+            {
+                x += move;
+            }
+            else if (e.KeyData == Keys.Left)
+            {
+                x -= move;
+            }
+            else if (e.KeyData == Keys.Up)
+            {
+                y -= move;
+            }
+            else if (e.KeyData == Keys.Down)
+            {
+                y += move;
+            }
+            else if(e.KeyData == Keys.C)
+            {
+                Graphics g = Graphics.FromImage(backImage);
+                g.Clear(Color.Yellow);
+            }
+            else if (e.KeyData == Keys.Escape)
+            {
+                EndSketching();
+            }
+
+            Refresh();
+        }
+
+        private void EndSketching()
+        {
+            if(MessageBox.Show("Do you want to finish?", "Message",
+                MessageBoxButtons.OKCancel) == DialogResult.OK)
+                Close();
+        }
+
+        private void SketchForm_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = Graphics.FromImage(backImage);
+            g.FillEllipse(myBrush, x, y, 20, 20);
         }
     }
 }
